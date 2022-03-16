48220eab0ca9ad7bf179bc2acc0bf9c686e0784f
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2022-03-16 17:37:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//Import packages
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get("/", (_, res) => {
+  res.send("C2S");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server started"));
+mongoose.connect(process.env.DB_CONNECTION, () => console.log("connected"));
